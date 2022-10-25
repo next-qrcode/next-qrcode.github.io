299c6df5c11876493372f63fe9e868298f46456f
Bunlong <bunlong.van@gmail.com>
2022-10-25 21:09:59 +0700

    Init

---
+++ _next/static/9nnrALAoc5TSg57LBby5N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/430-9970d5c841ca1a3a.js","static/chunks/pages/index-f3b682164a962b0f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
