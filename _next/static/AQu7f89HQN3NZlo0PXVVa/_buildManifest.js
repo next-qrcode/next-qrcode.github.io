6e54b88671ec595ac7e7009ffe983b53864e291c
Bunlong <bunlong.van@gmail.com>
2022-12-06 22:22:28 +0700

    Add logo

---
+++ _next/static/AQu7f89HQN3NZlo0PXVVa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/430-68ad0ee4eb91d03e.js","static/chunks/pages/index-ddec6408bad4da34.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/HU10YUtAAfQQQL6rH2qgF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/430-1f86615eb1d02770.js","static/chunks/pages/index-79a611a145b794e8.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
