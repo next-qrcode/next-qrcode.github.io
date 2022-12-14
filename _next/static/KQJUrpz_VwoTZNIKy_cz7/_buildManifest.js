61315d15e9f09505189de89b1bb6b178a475b8cd
Bunlong <bunlong.van@gmail.com>
2022-12-14 14:21:42 +0700

    Add next-prism

---
+++ _next/static/KQJUrpz_VwoTZNIKy_cz7/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/976-ca2603d158b75bf6.js","static/chunks/pages/index-24992442607a9366.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
